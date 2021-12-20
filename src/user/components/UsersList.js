d2d3850742c03d19afbf0cb877067aaff9c998ad
Shawn Kebel <fxrender@outlook.com>
2021-12-20 00:58:43 -0600

    :lipstick: Add Card to UsersList component

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import UserItem from './UserItem';
 import './UsersList.css';
+import Card from '../../shared/components/UIElements/Card';
 
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No users found</h2>
+                <Card>
+                   <h2>No users found</h2>
+                </Card>
             </div>
         );
     }
