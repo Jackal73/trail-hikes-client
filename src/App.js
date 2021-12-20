d61a2793fa92505781ac7424c7cc37be2a9ec0ce
Shawn Kebel <fxrender@outlook.com>
2021-12-20 00:34:42 -0600

    :sparkles: Add main header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Redirect, Route, Switch
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
 } from 'react-router-dom';
-import NewPlace from './hikes/pages/NewHike';
+import NewHike from './hikes/pages/NewHike';
 import Users from './user/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/hikes/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+    <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/hikes/new" exact>
+            <NewHike />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
