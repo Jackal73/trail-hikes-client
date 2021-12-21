e963a0873d9ce0fb41361df940e032dbf093916d
Shawn Kebel <fxrender@outlook.com>
2021-12-20 21:45:07 -0600

    :sparkles: Finish  `side drawer` - working `mobile` menu button

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import Backdrop from '../UIElements/Backdrop';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
 
-
-
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    }
+
   return (
     <React.Fragment>
-    <SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavLinks />
-      </nav>
-    </SideDrawer>
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-      <Link to="/">TrailHikes</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-  </MainHeader>
-  </React.Fragment>
+        {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+          <nav className="main-navigation__drawer-nav">
+              <NavLinks />
+          </nav>
+        </SideDrawer>
+        <MainHeader>
+          <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+            <span />
+            <span />
+            <span />
+          </button>
+          <h1 className="main-navigation__title">
+          <Link to="/">TrailHikes</Link>
+          </h1>
+          <nav className="main-navigation__header-nav">
+            <NavLinks />
+          </nav>
+      </MainHeader>
+    </React.Fragment>
 
   )
 };
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { CSSTransition } from 'react-transition-group';
 import './SideDrawer.css';
 
-const SideDrawer = props => {
-  const constant = <aside className="side-drawer">{props.children}</aside>
 
-  return ReactDOM.createPortal(constant, document.getElementById('drawer-hook'))
+const SideDrawer = props => {
+  const content = (
+  <CSSTransition
+   in={props.show}
+   timeout={200}
+   classNames="slide-in-left"
+   mountOnEnter
+   unmountOnExit
+  >
+    <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+  </CSSTransition>
+  );
+
+
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
 };
 
 export default SideDrawer;
