e19c9957e58ed70f7f7cb6a9474274cccd997481
Shawn Kebel <fxrender@outlook.com>
2021-12-20 19:00:56 -0600

    :construction: Add 'side drawer' to 'main navigation'

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
 import './MainNavigation.css';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+
+
 
 const MainNavigation = props => {
   return (
+    <React.Fragment>
+    <SideDrawer>
+      <nav className="main-navigation__drawer-nav">
+        <NavLinks />
+      </nav>
+    </SideDrawer>
     <MainHeader>
       <button className="main-navigation__menu-btn">
         <span />
@@ -16,10 +24,12 @@ const MainNavigation = props => {
       <h1 className="main-navigation__title">
       <Link to="/">TrailHikes</Link>
       </h1>
-      <nav>
-        <NavLinks />>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
       </nav>
   </MainHeader>
+  </React.Fragment>
+
   )
 };
 
