3af75c22c3b057f0c2ccff00d7cad35810d4d7a0
Shawn Kebel <fxrender@outlook.com>
2021-12-23 20:53:18 -0600

    :zap: Add multiple inputs and share input values

--- src/hikes/pages/NewHike.js
+++ src/hikes/pages/NewHike.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
+import { VALIDATOR_MAX, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
 import './NewHike.css';
 
 const NewHike = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
   return (
     <form className="hike-form">
       <Input
+      id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+       <Input
+       id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min 5 characters)."
+        onInput={descriptionInputHandler}
       />
   </form>
   );
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,33 +1,40 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../util/validators';
 import './Input.css';
 
 const inputReducer = (state, action) => {
     switch (action.type) {
-      case 'CHANGE':
-        return {
-          ...state,
-          value: action.val,
-          isValid: validate(action.val, action.validators)
-        };
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators)
+                };
         case 'TOUCH': {
             return {
                 ...state,
                 isTouched: true
             }
         }
-      default:
+    default:
         return state;
     }
-  };
+};
 
-  const Input = props => {
+const Input = props => {
     const [inputState, dispatch] = useReducer(inputReducer, {
-      value: '',
-      isTouched: false,
-      isValid: false
+        value: '',
+        isTouched: false,
+        isValid: false
     });
+
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput]);
 
     const changeHandler = event => {
         dispatch({
@@ -35,44 +42,44 @@ const inputReducer = (state, action) => {
             val: event.target.value,
             validators: props.validators
         });
-      };
+    };
 
-      const touchHandler = () => {
-          dispatch({
-              type: 'TOUCH'
+    const touchHandler = () => {
+        dispatch({
+            type: 'TOUCH'
         });
-      };
+    };
 
-  const element =
-    props.element === 'input' ? (
-      <input
-        id={props.id}
-        type={props.type}
-        placeholder={props.placeholder}
-        onChange={changeHandler}
-        onBlur={touchHandler}
-        value={inputState.value}
-      />
-    ) : (
-      <textarea
-        id={props.id}
-        rows={props.rows || 3}
-        onChange={changeHandler}
-        onBlur={touchHandler}
-        value={inputState.value}
-      />
-    );
+    const element =
+        props.element === 'input' ? (
+            <input
+                id={props.id}
+                type={props.type}
+                placeholder={props.placeholder}
+                onChange={changeHandler}
+                onBlur={touchHandler}
+                value={inputState.value}
+            />
+        ) : (
+            <textarea
+            id={props.id}
+            rows={props.rows || 3}
+            onChange={changeHandler}
+            onBlur={touchHandler}
+            value={inputState.value}
+            />
+        );
 
     return (
         <div
-          className={`form-control ${!inputState.isValid && inputState.isTouched &&
-            'form-control--invalid'}`}
-        >
-          <label htmlFor={props.id}>{props.label}</label>
-          {element}
-          {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+            className={`form-control ${!inputState.isValid && inputState.isTouched &&
+                'form-control--invalid'}`}
+            >
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+            {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
         </div>
-      );
+    );
 };
 
 export default Input;
