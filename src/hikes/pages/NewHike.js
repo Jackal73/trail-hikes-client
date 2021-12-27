059664dabb3778fb8f16bd3ef5a4ac19a07efd67
Shawn Kebel <fxrender@outlook.com>
2021-12-26 23:29:13 -0600

    :boom: Create custom Form Hook `useForm`

--- src/hikes/pages/NewHike.js
+++ src/hikes/pages/NewHike.js
@@ -1,38 +1,16 @@
-import React, { useCallback, useReducer } from 'react';
+import React from 'react';
 
 import Button from '../../shared/components/FormElements/Button';
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import './HikeForm.css';
 
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case'INPUT_CHANGE':
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: { value: action.value, isValid: action.isValid }
-        },
-        isValid: formIsValid
-      };
-    default:
-      return state;
-  }
-};
 
 const NewHike = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
         value: '',
         isValid: false
@@ -46,17 +24,10 @@ const NewHike = () => {
         isValid: false
       }
     },
-    isValid: false
-  });
+   false
+  );
 
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: 'INPUT_CHANGE',
-      value: value,
-      isValid: isValid,
-      inputId: id
-    });
-  }, []);
+
 
   const placeSubmitHandler = event => {
     event.preventDefault();
--- src/hikes/pages/UpdateHike.js
+++ src/hikes/pages/UpdateHike.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
-} from '../../shared/components/util/validators';
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import './HikeForm.css';
 
 const DUMMY_HIKES = [
@@ -37,20 +39,70 @@ const DUMMY_HIKES = [
 ];
 
 const UpdateHike = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const hikeId = useParams().hikeId;
 
+
+
+  const [formState, inputHandler, setFormData] =  useForm({
+    title: {
+      value: '',
+      isValid: false
+    },
+    description: {
+      value: '',
+      isValid: false
+    }
+  }, false
+  );
+
   const identifiedHike = DUMMY_HIKES.find(p => p.id === hikeId);
+
+
+  useEffect(() => {
+    if (identifiedHike) {
+
+    setFormData({
+      title: {
+        value: identifiedHike.title,
+        isValid: true
+      },
+      description: {
+        value: identifiedHike.description,
+        isValid: true
+      }
+    },
+    true
+    );
+  }
+    setIsLoading(false);
+  }, [setFormData, identifiedHike]);
+
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  }
 
   if (!identifiedHike) {
     return (
       <div className="center">
+        <Card>
         <h2>Could not find hike!</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading . . .</h2>
       </div>
     );
   }
 
   return (
-    <form className="hike-form">
+    <form className="hike-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -58,9 +110,9 @@ const UpdateHike = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
-        onInput={() => {}}
-        value={identifiedHike.title}
-        valid={true}
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
       />
       <Input
         id="description"
@@ -68,11 +120,11 @@ const UpdateHike = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (min. 5 characters)."
-        onInput={() => {}}
-        value={identifiedHike.description}
-        valid={true}
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
       />
-      <Button type="submit" disabled={true}>
+      <Button type="submit" disabled={!formState.isValid}>
         UPDATE HIKE
       </Button>
     </form>
