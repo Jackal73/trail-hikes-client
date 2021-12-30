54b13f6d387da72281fb113ba89e6685b1bf2667
Shawn Kebel <fxrender@outlook.com>
2021-12-29 18:22:12 -0600

    :zap: Create `auth-context` for 'app wide' state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -12,32 +12,45 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import Users from './user/pages/Users';
 import UpdateHike from './hikes/pages/UpdateHike';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-    <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/hikes" exact>
-            <UserHikes />
-          </Route>
-          <Route path="/hikes/new" exact>
-            <NewHike />
-          </Route>
-          <Route path="/hikes/:hikeId">
-            <UpdateHike />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}>
+      <Router>
+      <MainNavigation />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/:userId/hikes" exact>
+              <UserHikes />
+            </Route>
+            <Route path="/hikes/new" exact>
+              <NewHike />
+            </Route>
+            <Route path="/hikes/:hikeId">
+              <UpdateHike />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
