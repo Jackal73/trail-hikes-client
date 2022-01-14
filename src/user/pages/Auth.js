919c770fe1192ffd29e545f37aa98a1d21a0b18d
Shawn Kebel <fxrender@outlook.com>
2022-01-13 21:08:10 -0600

    :sparkles: Add GET request and use `http-hook` to GET users

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -6,6 +6,7 @@ import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from '../../shared/context/auth-context';
 import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
@@ -17,8 +18,7 @@ import './Auth.css';
 const Auth = () => {
     const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -61,67 +61,49 @@ const Auth = () => {
     const authSubmitHandler = async event => {
         event.preventDefault();
 
-        setIsLoading(true);
-
         if (isLoginMode) {
             try {
-                const response = await fetch('http://localhost:5000/api/users/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        email: formState.inputs.email.value,
-                        password: formState.inputs.password.value
-                    })
-                });
-
-                const responseData = await response.json();
-                if (!response.ok) {
-                    throw new Error(responseData.message);
+                await sendRequest(
+                    'http://localhost:5000/api/users/login',
+                    "POST",
+                JSON.stringify({
+                    email: formState.inputs.email.value,
+                    password: formState.inputs.password.value
+                }),
+        {
+                    'Content-Type': 'application/json'
                 }
-                setIsLoading(false);
+
+                );
                 auth.login();
             } catch (err) {
-                console.log(err);
-                setIsLoading(false);
-                setError(err.message || 'Something went wrong, please try again.');
+
             }
         } else {
             try {
-                const response = await fetch('http://localhost:5000/api/users/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
+                await sendRequest(
+                    'http://localhost:5000/api/users/signup',
+                    'POST',
+                    JSON.stringify({
                         name: formState.inputs.name.value,
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value
-                    })
-                });
+                }),
+                    {
+                        'Content-Type': 'application/json'
+                    }
+                );
 
-                const responseData = await response.json();
-                if (!response.ok) {
-                    throw new Error(responseData.message);
-                }
-                setIsLoading(false);
                 auth.login();
-            } catch (err) {
-                console.log(err);
-                setIsLoading(false);
-                setError(err.message || 'Something went wrong, please try again.');
-            }
+            } catch (err) {}
         }
     };
 
-    const errorHandler = () => {
-        setError(null);
-    };
+
 
     return (
         <React.Fragment>
-            <ErrorModal error={error} onClear={errorHandler} />
+            <ErrorModal error={error} onClear={clearError} />
             <Card className="authentication">
                 {isLoading && <LoadingSpinner asOverlay />}
                 <h2>Login Required</h2>
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,18 +1,44 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import UsersList from '../components/UsersList';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Shawn Kebel',
-      image: 'https://images.pexels.com/photos/1311523/pexels-photo-1311523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      hikes: 3
-     }
-  ];
+  const {isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const fetchUsers = async () => {
+
+      try {
+        const responseData = await sendRequest('http://localhost:5000/api/users');
+
+
+
+
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
 };
 
 export default Users;
