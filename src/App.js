b386e9f2ad273512a8abcf0ffd2c1c5c4c5cc0cd
Shawn Kebel <fxrender@outlook.com>
2022-01-21 17:54:32 -0600

    :boom::children_crossing: Store `token` in 'local storage' - `auto-login`

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch
 } from 'react-router-dom';
-
 import NewHike from './hikes/pages/NewHike';
+import UpdateHike from './hikes/pages/UpdateHike';
 import UserHikes from './hikes/pages/UserHikes';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Users from './user/pages/Users';
-import UpdateHike from './hikes/pages/UpdateHike';
-import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
+import Auth from './user/pages/Auth';
+import Users from './user/pages/Users';
+
 
 const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-
   const login = useCallback((uid, token) => {
     setToken(token);
-    setUserId(uid)
+    setUserId(uid);
+    localStorage.setItem('userData', JSON.stringify({ userId: uid, token: token})
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
   let routes;
 
